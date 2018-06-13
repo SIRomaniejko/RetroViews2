543d39e42a3226b7848bc3e02305f2a3c9ca513e
SIRomaniejko <siromaniejko@gmail.com>
2018-06-13 13:08:48 -0300

    grid

--- js/ajax.js
+++ js/ajax.js
@@ -1,8 +1,37 @@
 let container = document.querySelector(".js-container");
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", home);
+document.querySelector(".flex-nav").querySelectorAll("div")[0].addEventListener("click", home);
+
+function home(){
     fetch("home.html").then(respuesta=>{
         console.log("funciona");
-        respuesta.text().then(regreso=>
-            container.innerHTML = regreso)
+        respuesta.text().then(regreso=>{
+            console.log("funcionax2");
+            container.innerHTML = regreso;
+            let divs = document.querySelector(".container").querySelectorAll("div");
+            for(a of divs){
+                let p = divs[a].getElementsByTagName("p")[0];
+                agregarResena(p, divs[a]);
+            }
+        })
     })
-})
+}
+
+function posts(){
+
+}
+
+function subscripccion(){
+
+}
+
+function agregarResena(p, div){
+    div.addEventListener("click", function(){
+        fetch("resenas/" + p).then(respuesta =>{
+            respuesta.text().then(texto=>{
+                container.innerHTML = texto;
+            })
+        })
+    })
+}
+
