fc4ba22c37694730c9d6af5da3acbf26a624df61
SIRomaniejko <siromaniejko@gmail.com>
2018-06-20 16:00:32 -0300

    tabla funcional

--- js/ajax.js
+++ js/ajax.js
@@ -67,14 +67,21 @@ function agregarAJAXArticulos(){
 let URLRest = "https://web-unicen.herokuapp.com/api/groups/RomaYOli/resenas";
 
 function creaTuReview(){
-    document.querySelector("#enviar").addEventListener("click", ()=>{
+    let inputs = document.querySelectorAll("input");
+    document.querySelector("#enviar").addEventListener("click", enviarData);
+    document.querySelector("#enviarX3").addEventListener("click", ()=>{
+        for(let a = 0; a < 3; a++){
+            enviarData();
+        }
+    })
+    function enviarData(){
+        agregarElemento();
         console.log("funciona?")
-        let inputs = document.querySelectorAll("input");
         let data = {
             "thing": {
                 "nombre": inputs[0].value,
                 "graficos": inputs[1].value,
-                "banda sonora": inputs[2].value,
+                "bandaSonora": inputs[2].value,
                 "gameplay": inputs[3].value,
                 "argumento": inputs[4].value
             }
@@ -84,8 +91,63 @@ function creaTuReview(){
             "headers": {"Content-Type": "application/json"},
             "body": JSON.stringify(data)
         })
-    })
-    function borrarPagina(ultraURL){
+    }
+    refresh();
+    function refresh(){
+        console.log("6 veces?");
+        fetch(URLRest).then(resp=>{
+            resp.json().then(objeto=>{
+                objeto.resenas.forEach(reviews=>{
+                    valores = reviews.thing;
+                    let nuevaRow = crearFila(false, valores);
+                    document.querySelector("tbody").appendChild(nuevaRow);
+                })
+            })
+        })
+    }
+    function totalTd(arr){
+        let total = 0;
+        for(let a = 1; a < 5; a++){
+            total += parseFloat(arr[a].innerHTML, 10);
+        }
+        total = total/4;
+        let regreso = document.createElement("td");
+        regreso.innerHTML = total;
+        return regreso;
+    }
+    function crearFila(esLocal, valores){
+        let nuevaFila = document.createElement("tr");
+        let td = [];
+        if(esLocal==true){
+            let a = 0;
+            inputs.forEach(input=>{
+                td[a] = document.createElement("td");
+                td[a].innerHTML = input.value;
+                nuevaFila.appendChild(td[a]);
+                a++;
+            })
+            
+        }
+        else{
+            let a = 0;
+            arrValores = [valores.nombre, valores.graficos, valores.bandaSonora, valores.gameplay, valores.argumento];
+            arrValores.forEach(valorReview=>{
+                td[a] = document.createElement("td");
+                td[a].innerHTML = valorReview;
+                nuevaFila.appendChild(td[a]);
+                a++;
+            })
+        }
+        tdTotal = totalTd(td);
+        nuevaFila.appendChild(tdTotal);
+        return nuevaFila;
+    }
+    function agregarElemento(){
+        let nuevaRow = crearFila(true);
+        document.querySelector("tbody").appendChild(nuevaRow);
+    }
+
+    function borrarRest(ultraURL){
         ultraURL = URLRest + "/" + ultraURL;
         fetch(ultraURL, {
             "method": "DELETE",
@@ -93,12 +155,14 @@ function creaTuReview(){
             "headers": {"Content-Type": "application/json"}
         })
     }
+    
+    document.querySelector("#pija").addEventListener("click", aLaVerga);
     function aLaVerga(){
         fetch(URLRest).then(resp=>{
             resp.json().then(a=>{
                 a.resenas.forEach(reviews =>{
                     console.log(reviews._id);
-                    borrarPagina(reviews._id);
+                    borrarRest(reviews._id);
                 })
             })
         })
